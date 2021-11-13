4d40d659181881880627f8230783622e7fe42210
Elang89 <elang.apps@gmail.com>
2021-11-13 12:09:11 -0500

    emergency commit

--- main.py
+++ main.py
@@ -1,122 +1,39 @@
-import json
+import shutil
 
 from glob import glob
 from pprint import pprint
 from multiprocessing import Process
-from typing import Dict, List, Union, Tuple
-from csv import DictWriter
+from uuid import uuid4
 
-#TODO: features and reviews
 
-def extract_splits(json_data: Dict[str, str]) -> Dict[str, str]: 
-    train = json_data.get("train", False)
-    test = json_data.get("test", False)
-    validation = json_data.get("validation", False)
-    data = {}
 
-    if train:
-        data.update({"has_train_split": 1})
-    else:
-        data.update({"has_train_split": 0})
-    
-    if test: 
-        data.update({"has_test_split": 1})
-    else:
-        data.update({"has_test_split": 0})
-
-    if validation: 
-        data.update({"has_validation_split": 1})
-    else: 
-        data.update({"has_validation_split": 0})
+def copy_files(arrow_paths):
+    for path in arrow_paths: 
+        path_splits = path.split("/")
+        current_file = path_splits[-1]
+        config = path_splits[-4]
         
-
-    return data
-
-
-def extract_citation_information(json_data: str):
-    pass
-
-
-def check_versioning(json_data: Union[dict, str]) -> str:
-    version = json_data
-
-    if isinstance(json_data, str): 
-        return version
-    elif isinstance(version, dict):
-        version_str = version.get("version_str")
-        return version_str
-
-
-def analyze_jsons() -> None: 
-
-    field_names = [
-            "builder_name", 
-            "config_name",
-            "is_config",
-            "description_size",
-            "download_size(bytes)", 
-            "has_homepage",
-            "version",
-            "has_license",
-            "has_train_split",
-            "has_test_split",
-            "has_validation_split"
-    ]
-    result_file = open("data.csv", "w")
-
-    writer = DictWriter(result_file, fieldnames=field_names)
-    writer.writeheader()
-
-    with open("jsons.txt", "r") as file:
-        results = file.readlines()
-
-        for result in results:
-            json_file_path = result.strip("\n")
-            with open(json_file_path, "r") as jsonfile:
-                loaded_json = json.load(jsonfile)
-                description_size = len(loaded_json["description"])
-                has_version = check_versioning(loaded_json["version"])
-                splits = extract_splits(loaded_json["splits"])
-
-                row = {
-                    "builder_name": loaded_json.get("builder_name"),
-                    "config_name": loaded_json.get("config_name"), 
-                    "is_config": 1 if loaded_json.get("config_name") != "default" else 0,
-                    "description_size": description_size,
-                    "download_size(bytes)": loaded_json.get("download_size"),
-                    "has_homepage": 1 if loaded_json.get("homepage") else 0,
-                    "version": has_version,
-                    "has_license": 1 if loaded_json.get("license") else 0,
-                    "has_train_split": splits.get("has_train_split"),
-                    "has_test_split": splits.get("has_test_split"),
-                    "has_validation_split": splits.get("has_validation_split")
-                }
-
-                writer.writerow(row)
-    result_file.close()
-
-
+        filename = f"{uuid4()}-{config}-{current_file}"
+        
+        current_path = f"comparisons/{filename}"
+        
+        print(filename)
+        
+        shutil.copyfile(path, current_path)
 
 
 def main() -> None: 
-    # jsons = "/mnt/e/Data/HFDatasets/**/dataset_info.json"
-    # arrows = "/mnt/e/Data/HFDatasets/**/*.arrow"
+    arrows = "/mnt/e/HFDatasets/**/*train*.arrow"
+    arrow_paths = glob(arrows, recursive=True)[760:]
 
-    # arrow_results = glob(arrows, recursive=True)
-    # json_results = glob(jsons, recursive=True)
-
-    # with open("arrows.txt", "w") as file:
-    #     for x in arrow_results:
-    #         str_to_write = f"{x}\n"
-    #         file.write(str_to_write)
-
-    # with open("jsons.txt", "w") as file: 
-    #     for y in json_results:
-    #         str_to_write = f"{y}\n"
-    #         file.write(str_to_write)
-
-    analyze_jsons()
-
+    
+    arrow_paths = [arrow_paths[x:x + 80] for x in range(0, len(arrow_paths), 80)]
+    
+    producers = [Process(target=copy_files, args=(arrow_paths[x],)) for x in range(0, len(arrow_paths))]
+    
+    for producer in producers:
+        producer.start()
+    
 
 
 if __name__ == "__main__":
